48b49160c41cd430ab50eba9e699e9aab84a0a6f
Fabrizio Bagalà <62190484+fabriziobagala@users.noreply.github.com>
2024-04-26 16:59:31 +0200

    Delete src/SQL/Basic Select/sqlserver/Weather Observation Station 08 (InOperator).sql

--- src/SQL/Basic Select/sqlserver/Weather Observation Station 08 (InOperator).sql
+++
@@ -1,4 +0,0 @@
-SELECT DISTINCT City
-FROM Station
-WHERE LEFT(City, 1) IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u') AND
-      RIGHT(City, 1) IN ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u');
